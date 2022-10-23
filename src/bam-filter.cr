b7f49333bf6d2dd46db7dc1ed7712fe6bb17e10a
kojix2 <2xijok@gmail.com>
2022-10-24 00:25:36 +0900

    Allow aux tags and improved error messages

--- src/bam-filter.cr
+++ src/bam-filter.cr
@@ -6,8 +6,12 @@ require "option_parser"
 require "./ke"
 require "hts/bam"
 
-PROGRAM = "bam-filter"
-VERSION = "0.0.6"
+PROGRAM    = "bam-filter"
+VERSION    = "0.0.6"
+FLAG_NAMES = \
+   %w[paired proper_pair unmapped mate_unmapped
+  reverse mate_reverse read1 read2
+  secondary qcfail duplicate supplementary]
 
 expr = ""
 debug = false
@@ -16,6 +20,11 @@ nthreads = 0
 input_file = ""
 output_file = "-"
 output_format = ""
+
+count = 0
+tags = [] of String
+
+# Option Parser
 
 OptionParser.parse do |parser|
   parser.banner = <<-EOS
@@ -30,6 +39,7 @@ OptionParser.parse do |parser|
   parser.on("-S", "--sam", "Output SAM") { output_format = ".sam" }
   parser.on("-b", "--bam", "Output BAM") { output_format = ".bam" }
   # parser.on("-f", "--fasta PATH") { |v| p v }
+  parser.on("--debug", "Debug mode") { debug = true }
   parser.on("-h", "--help", "Show this help") do
     puts parser
     exit
@@ -58,20 +68,22 @@ OptionParser.parse do |parser|
   end
 end
 
-input_file = ARGV[0]
+# Check arguments
 
-if input_file == ""
-  STDERR.puts "ERROR: bam file is not specified."
+if ARGV.empty?
+  STDERR.puts "[bam-filter] ERROR: input file is not specified."
   exit(1)
 end
 
+input_file = ARGV[0]
+
 unless File.exists?(input_file)
-  STDERR.puts "ERROR: #{input_file} not found."
+  STDERR.puts "[bam-filter] ERROR: #{input_file} not found."
   exit(1)
 end
 
 if expr == ""
-  STDERR.puts "ERROR: no expression specified for -e"
+  STDERR.puts "[bam-filter] ERROR: no expression specified for -e"
   exit(1)
 end
 
@@ -87,16 +99,7 @@ if output_format == ""
   end
 end
 
-e = KE.new(expr)
-bam = HTS::Bam.open(input_file, threads: nthreads)
-mode = (output_format == ".sam" ? "w" : "wb")
-bam_out = HTS::Bam.open(output_file, mode)
-bam_out.write_header(bam.header)
-
-FLAG_NAMES = \
-   %w[paired proper_pair unmapped mate_unmapped
-  reverse mate_reverse read1 read2
-  secondary qcfail duplicate supplementary]
+# Check the fields to be used before execution.
 
 use : Hash(String, Bool)
 {% begin %}
@@ -115,8 +118,35 @@ use = {
 }
 {% end %}
 
+# Check the tags to be used before execution.
+
+expr.scan(/(?<=tag_)[A-Za-z0-9]+/).each do |md|
+  t = md[0]
+  if t.size == 2
+    tags << t
+  else
+    STDERR.puts "[bam-filter] ERROR: Incorrect tag name. #{t}"
+    exit(1)
+  end
+end
+
+# Main
+
+begin
+  e = KE.new(expr)
+rescue ex : Exception
+  STDERR.puts "[bam-filter] ERROR: failed to parse expression. #{ex.message}"
+  exit(1)
+end
+
+bam = HTS::Bam.open(input_file, threads: nthreads)
+mode = (output_format == ".sam" ? "w" : "wb")
+bam_out = HTS::Bam.open(output_file, mode)
+bam_out.write_header(bam.header)
+
 bam.each do |r|
   e.clear
+  # Fields
   e.set("mapq", r.mapq) if use["mapq"]
   e.set("start", r.pos) if use["start"]
   e.set("pos", r.pos + 1) if use["pos"]
@@ -125,12 +155,32 @@ bam.each do |r|
   e.set("mpos", r.mate_pos) if use["mpos"]
   e.set("isize", r.insert_size) if use["isize"]
   e.set("flag", r.flag.value) if use["flag"]
+  # Flags
   {% for name in FLAG_NAMES %}
     e.set("{{name.id}}", (r.flag.{{name.id}}? ? 1 : 0)) if use["{{name.id}}"]
   {% end %}
+  # Auxiliary data
+  tags.each do |t|
+    v = r.aux(t)
+    e.set("tag_#{t}", v) unless v.nil?
+  end
 
-  bam_out.write(r) if e.bool
+  # Write
+  if e.bool
+    if e.error_code == 0
+      bam_out.write(r)
+      count += 1
+    else
+      STDERR.puts "[bam-filter] #{e.eval_error}" if debug
+    end
+  end
 end
 
 bam.close
 bam_out.close
+
+if count == 0
+  STDERR.puts "[bam-filter] No records were written."
+else
+  STDERR.puts "[bam-filter] Wrote #{count} records."
+end
--- src/ke.cr
+++ src/ke.cr
@@ -1,9 +1,26 @@
 require "./kexpr"
 
 class KE
+  # Parse errors
+  KEE_UNQU = 0x01 # unmatched quotation marks
+  KEE_UNLP = 0x02 # unmatched left parentheses
+  KEE_UNRP = 0x04 # unmatched right parentheses
+  KEE_UNOP = 0x08 # unknown operators
+  KEE_FUNC = 0x10 # wrong function syntax
+  KEE_ARG  = 0x20
+  KEE_NUM  = 0x40 # fail to parse a number
+
+  # Evaluation errors
+  KEE_UNFUNC = 0x40 # undefined function
+  KEE_UNVAR  = 0x80 # unassigned variable
+
   def initialize(s : String)
     @err = Pointer(Int32).malloc
     @ke = Kexpr.parse(s, @err)
+    err = @err.value
+    if err != 0
+      raise "#{parse_error(err)} (#{err})"
+    end
   end
 
   def clear
@@ -23,27 +40,61 @@ class KE
     Kexpr.eval_real(@ke, @err).abs > 1e-8
   end
 
-  def error
+  def error_code
     @err.value
   end
 
-  def set(name : String, v : (Int8 | UInt8 | Int16 | UInt16 | Int32 | UInt32 | Int64| UInt64))
+  def parse_error(err = error_code)
+    if err == 0
+      return "no error"
+    elsif (err & KEE_UNQU) != 0
+      "unmatched quotation marks"
+    elsif (err & KEE_UNLP) != 0
+      "unmatched left parentheses"
+    elsif (err & KEE_UNRP) != 0
+      "unmatched right parentheses"
+    elsif (err & KEE_UNOP) != 0
+      "unknown operators"
+    elsif (err & KEE_FUNC) != 0
+      "wrong function syntax"
+    elsif (err & KEE_ARG) != 0
+      "wrong arguments"
+    elsif (err & KEE_NUM) != 0
+      "fail to parse a number"
+    else
+      "unknown error"
+    end
+  end
+
+  def eval_error(err = error_code)
+    if err == 0
+      return "no error"
+    elsif (err & KEE_UNFUNC) != 0
+      "undefined function"
+    elsif (err & KEE_UNVAR) != 0
+      "unassigned variable"
+    else
+      "unknown error"
+    end
+  end
+
+  def set(name : String, v : (Int8 | UInt8 | Int16 | UInt16 | Int32 | UInt32 | Int64 | UInt64))
     Kexpr.set_int(@ke, name, v)
-    error
   end
 
   def set(name : String, v : (Float32 | Float64))
     Kexpr.set_real(@ke, name, v)
-    error
   end
 
   def set(name : String, v : String)
     Kexpr.set_str(@ke, name, v)
-    error
   end
 
   def set(name : String, v : Char)
     Kexpr.set_str(@ke, name, v.to_s)
-    error
+  end
+
+  def print
+    Kexpr.print(@ke)
   end
 end
